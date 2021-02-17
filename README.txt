01e0a4c405f9a3e6a0bb1613bf5f756fbc73de70
mibre2 <mibre2@morgan.edu>
2021-02-17 15:57:42 -0500

    Commented original code and added a README file.

---
+++ README.txt
@@ -0,0 +1,18 @@
+## PyTrade
+
+### User Stories
+
+#### REQUIRED
+
+- [x] Expose details of stocks with minute intervals
+- [] Allow user to buy and sell stocks
+
+#### BONUS
+
+- [ ] Create a custom GUI
+
+# TDAmeritrade
+# PyTrading
+PyTrade is an app that allows users to autonomously interact with the stock market through [TDAmeritrade API](https://developer.tdameritrade.com/apis).
+
+#### NOTES
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
+# Allows app to send HTTP request using python
 import requests
 
-# API KEY for app
+# String that represents API KEY
 key = '8ZQFGWTUEHSHIPYXG9THFNOBCESYYTKT'
 
-
+# Get price history from market
 def get_price_history(**kwargs):
     url = 'https://api.tdameritrade.com/v1/marketdata/{}/pricehistory'.format(kwargs.get('symbol'))
 
     params = {}
     params.update({'apikey': key})
 
+    # Obtaining the args key and value
     for arg in kwargs:
         parameter = {arg: kwargs.get(arg)}
+        # Updates parameters
         params.update(parameter)
 
     return requests.get(url, params=params).json()
 
 
-# Prints price of symbol per minute throughout the day
+# Prints stock data throughout day in minute intervals
 print(get_price_history(symbol='AMZN', period=1, periodType='day', frequencyType='minute'))
