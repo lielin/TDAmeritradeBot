161f41cb6b4c4be0abf5c47bfd72923937c217de
mibre2 <mibre2@morgan.edu>
2021-02-20 11:30:24 -0500

    Create symbols for dictionary use Add Position as dictionary Add multiple positions as dictionary Remove position as dictionary

---
+++ portfolio.py
@@ -0,0 +1,83 @@
+from typing import Tuple
+from typing import List
+from typing import Dict
+from typing import Union
+from typing import Optional
+
+
+class Portfolio:
+
+    # Initial values for portfolio
+    def __init__(self, account_number: Optional[str]):
+        self.positions = {}  # Houses all positions
+        self.positions_count = 0
+        self.market_value = 0.0
+        self.profit_loss = 0.0
+        self.risk_tolerance = 0.0  # Risk tolerance for trading portfolio
+        self.account_number = account_number  # Used for API calls
+
+    # User adds one position
+    def add_position(self, symbol: str, asset_type: str, purchase_date: Optional[str], quantity: int = 0,
+                     purchase_price: float = 0.0) -> dict:
+        # Call positions dictionary
+        # Add a key to it (symbol)
+        # Dictionary is associated with key (Nested dictionary)
+        self.positions[symbol] = {}
+        self.positions[symbol]['symbol'] = symbol
+        self.positions[symbol]['quantity'] = quantity
+        self.positions[symbol]['purchase_price'] = purchase_price
+        self.positions[symbol]['purchase_date'] = purchase_date
+        self.positions[symbol]['asset_type'] = asset_type
+
+        # Return position dict back to user
+        return self.positions
+
+    # User adds multiple position at once
+    # MUST pass through as dictionary
+    def add_positions(self, positions: List[dict]) -> dict:
+
+        if isinstance(positions, List):
+            for position in positions:
+                self.add_position(
+                    symbol=position['symbol'],
+                    asset_type=position['asset_type'],
+                    purchase_date=position.get('purchase_date', None),
+                    purchase_price=position.get('purchase_price', 0.0),
+                    quantity=position.get('quantity', 0)
+                )
+
+                return self.positions
+        else:
+            raise TypeError("Positions must be a list of dictionaries")
+
+    def remove_position(self, symbol: str) -> Tuple[bool, str]:
+
+        if symbol in self.positions:
+            del self.positions[symbol]
+            return True, "{Symbol} was successfully removed.".format(symbol=symbol)
+        else:
+            return False, "{Symbol} did not exist in the portfolio.".format(symbol=symbol)
+
+    def in_portfolio(self, symbol: str) -> bool:
+        if symbol in self.positions:
+            return True
+        else:
+            return False
+
+    def is_profitable(self, symbol: str, current_price: float) -> bool:
+        # Get purchase price
+        purchase_price = self.positions[symbol]['purchase_price']
+
+        if purchase_price <= current_price:
+            return True
+        elif purchase_price > current_price:
+            return False
+
+    def total_allocation(self):
+        pass
+
+    def risk_exposure(self):
+        pass
+
+    def total_market_value(self):
+        pass
